03050656eb3ba42647339e52a10032b4b63b5ac3
Ryan Kelly <ryantkelly@gmail.com>
2017-12-29 19:13:44 -0500

    First done

---
+++ viddler.py
@@ -0,0 +1,171 @@
+import requests, json, os, pprint, csv, datetime
+from furl import furl
+
+class Viddler():
+    viddler = "https://api.viddler.com/api/v2/"
+    
+    video_file = 'videos.json'
+    auth_file = 'auth.json'
+    meta_file = 'video_info.csv'
+    progress_file = 'progress.json'
+
+    def __init__(self, api_key, user, password, save_dir):
+        self.api_key = api_key
+        self.save_dir = save_dir
+        self.user = user
+        self.password = password
+        if not os.path.isdir(self.save_dir):
+            os.makedirs(self.save_dir)
+
+
+        if os.path.isfile(self.auth_file):
+            self.auth = json.load(open(self.auth_file))
+        else:
+            self.authenticate()
+
+        if os.path.isfile(self.video_file):
+            self.videos = json.load(open(self.video_file))
+        else:
+            self.videos = self.getVideos()
+
+    def authenticate(self):
+        """Authenticates and saves token to auth.json file"""
+        params = {
+            'user': self.user,
+            'password': self.password,
+            'get_record_token': 1,
+            'api_key': self.api_key
+        }
+        url = furl(self.viddler+'viddler.users.auth.json').add(params)
+        try:
+            auth = requests.get(url)
+        except:
+            raise
+        self.auth = auth.json()
+        self.saveJson(self.auth_file, auth.json())
+        
+        
+    def saveJson(self, _file, _dict):
+        """Function to write json"""
+        with open(_file, 'w') as f:
+            f.write(json.dumps(_dict))
+
+    def getVideos(self):
+        """Gets all videos for account, and writes to videos.json"""
+        f = open(self.video_file, 'w')
+        _list = []
+        params = {
+            'sessionid': self.auth['auth']['sessionid'],
+            'api_key': self.api_key,
+            'user': self.user,
+            'per_page': 100
+        }
+        params2 = params
+
+        page = 0
+        data = True
+        while data == True:
+            page = page + 1
+            params2['page'] = page
+            url = furl(self.viddler+'viddler.videos.getByUser.json').add(params2)
+            try:
+                result = requests.get(url)
+            except:
+                raise
+            result.raise_for_status()
+            if result.json()['list_result']['video_list']:
+                for video in result.json()['list_result']['video_list']:
+                    _list.append(video)
+            else:
+                data = False
+        f.write(json.dumps(_list))
+        return json.load(open(self.video_file))
+
+    def makePublic(self, _id):
+        """Make a video publically downlodable"""
+        params = {
+            'sessionid': self.auth['auth']['sessionid'],
+            'api_key': self.api_key,
+            'video_id': _id,
+            'download_perm': 'public'
+        }
+        url = furl(self.viddler+'viddler.videos.setDetails.json').add(params)
+        try:
+            result = requests.post(url)
+        except:
+            raise
+        try:
+            result.raise_for_status()
+        except requests.exceptions.HTTPError:
+            self.authenticate()
+
+    def makePrivate(self, _id):
+        """Make video private again"""
+        params = {
+            'sessionid': self.auth['auth']['sessionid'],
+            'api_key': self.api_key,
+            'video_id': _id,
+            'download_perm': 'private'
+        }
+        url = furl(self.viddler+'viddler.videos.setDetails.json').add(params)
+        try:
+            result = requests.post(url)
+        except:
+            raise
+        try:
+            result.raise_for_status()
+        except requests.exceptions.HTTPError:
+            self.authenticate()
+
+    def writeProgress(self, _id, status):
+        """Write the progress (complete, failed, other) to json"""
+        try:
+            with open(self.progress_file, 'r+') as f:
+                d = json.loads(f.read())
+                d.append({_id: status})
+            with open(self.progress_file, 'w') as f:
+                f.write(json.dumps(d))
+        except FileNotFoundError:
+            with open(self.progress_file, 'w') as f:
+                d = [{_id: status}]
+                f.write(json.dumps(d))
+
+    def downloadVideo(self, video):
+        """Download a video from a json object"""
+        self.makePublic(video['id'])
+        for _file in video['files']:
+            if _file['profile_name'] == "Source":
+                dest = os.path.join(self.save_dir, video['id']+'.'+_file['ext'])
+                try:
+                    result = requests.get(_file['url'], stream=True)
+                except:
+                    raise
+                try:
+                    result.raise_for_status()
+                except requests.exceptions.HTTPError:
+                    self.authenticate()
+
+                with open(dest, 'wb') as f:
+                    for chunk in result.iter_content(chunk_size=1024):
+                        if chunk:
+                            f.write(chunk)
+                self.makePrivate(video['id'])
+                self.saveVideoMeta(video)
+                self.writeProgress(video['id'], 'complete')
+                return True
+    
+    def saveVideoMeta(self, video):
+        dest = os.path.join(self.save_dir, self.meta_file)
+        if not os.path.isfile(dest):
+            with open(dest, 'w') as f:
+                writer = csv.writer(f)
+                writer.writerow(['Video ID', 'Directory', 'Filename', 'Title', 'Description', 'Published', 'View Count','Impression Count'])
+        
+        directory = 'videos'
+        for _file in video['files']:
+            if _file['profile_name'] == "Source":
+                break
+        pub = datetime.datetime.fromtimestamp(int(video['made_public_time'])).strftime('%Y-%m-%d %H:%M:%S')
+        with open(dest, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([video['id'], directory, video['id']+'.'+_file['ext'], video['title'], video['description'], pub, video['view_count'], video['impression_count']])
